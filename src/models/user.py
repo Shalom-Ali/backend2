d49eebbde556adaeafc415800f9297bd6457c304
Shalom-Ali <m80009119@zu.ac.ae>
2025-04-25 22:05:17 +0400

    Update user.py

--- src/models/user.py
+++ src/models/user.py
@@ -1,8 +1,7 @@
 from pydantic import BaseModel
-from typing import List, Dict
 
 class User(BaseModel):
     id: str
     email: str
-    progress: Dict[str, float]  # e.g., {"module1": 0.75, "module2": 0.20}
-    preferences: List[str]      # e.g., ["math", "science"]
+    progress: dict
+    preferences: list
