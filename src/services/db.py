ae74643b8474ec1d28c81f8c9c0cffe3d9bae73d
Shalom-Ali <m80009119@zu.ac.ae>
2025-04-25 22:07:42 +0400

    Update db.py

--- src/services/db.py
+++ src/services/db.py
@@ -1,4 +1,5 @@
 from azure.cosmos import CosmosClient
+from azure.cosmos.exceptions import CosmosResourceNotFoundError
 from dotenv import load_dotenv
 import os
 
@@ -6,17 +7,25 @@ load_dotenv()
 
 class CosmosDB:
     def __init__(self):
-        self.client = CosmosClient(
-            url=os.getenv("COSMOS_ENDPOINT"),
-            credential=os.getenv("COSMOS_KEY")
-        )
-        self.database = self.client.get_database_client(os.getenv("DATABASE_NAME"))
-        self.container = self.database.get_container_client(os.getenv("CONTAINER_NAME"))
+        endpoint = os.getenv("COSMOS_ENDPOINT")
+        key = os.getenv("COSMOS_KEY")
+        if not endpoint or not key:
+            raise ValueError("COSMOS_ENDPOINT and COSMOS_KEY must be set")
+        self.client = CosmosClient(url=endpoint, credential=key)
+        self.database = self.client.get_database_client(os.getenv("DATABASE_NAME", "LearningDB"))
+        self.container = self.database.get_container_client(os.getenv("CONTAINER_NAME", "Users"))
 
     def get_user(self, user_id: str):
-        query = f"SELECT * FROM c WHERE c.id = '{user_id}'"
-        items = list(self.container.query_items(query=query, enable_cross_partition_query=True))
-        return items[0] if items else None
+        try:
+            query = "SELECT * FROM c WHERE c.id = @user_id"
+            items = list(self.container.query_items(
+                query=query,
+                parameters=[{"name": "@user_id", "value": user_id}],
+                enable_cross_partition_query=True
+            ))
+            return items[0] if items else None
+        except CosmosResourceNotFoundError:
+            return None
 
     def upsert_user(self, user: dict):
         self.container.upsert_item(user)
@@ -26,3 +35,5 @@ class CosmosDB:
         if user:
             user["progress"][module] = progress
             self.upsert_user(user)
+            return user
+        return None
